3574b5bdae84c19c537de35c2989eadd7afad47a
ArielAyala <arielayalamail@gmail.com>
2020-10-30 20:07:14 -0300

    Agregado de pruestas de get con parametros

--- server/server.spec.js
+++ server/server.spec.js
@@ -24,6 +24,7 @@ describe("Testing Router", () => {
   });
 
   describe("GET", () => {
+
     // GET 200
     it("200 and find pin", (done) => {
       const data = [{ id: 1 }];
@@ -39,5 +40,58 @@ describe("Testing Router", () => {
     });
 
     // GET 500
+    it("500", (done) => {
+      const data = [{ id: 1 }];
+      spyOn(Pins, "find").and.callFake((callBack) => {
+        callBack(true, data);
+      });
+
+      request.get("http://localhost:3000/api", (error, response, body) => {
+        expect(response.statusCode).toBe(500);
+        done();
+      });
+    });
+
+    // Get 200 FindById
+    it("200 and findById pin", (done) => {
+      const data = { id: 1 };
+      /* Dentro del Objeto Pins queremos espiar el metodo findById y ejecutamos nuestro propio metodo */
+      spyOn(Pins, "findById").and.callFake((id, callBack) => {
+        data.param = id;
+        callBack(false, data);
+      });
+
+      request.get(
+        "http://localhost:3000/api/21312",
+        (error, response, body) => {
+          expect(response.statusCode).toBe(200);
+          expect(error).toBe(null);
+          expect(JSON.parse(response.body)).toEqual({ id: 1, param: "21312" });
+          expect(JSON.parse(response.body).id).toEqual(1);
+          expect(JSON.parse(body).param).toEqual("21312");
+          done();
+        }
+      );
+    });
+
+    //GET 500 FindById
+    it("500", (done) => {
+      const data = [{ id: 1 }];
+      /* Dentro del Objeto Pins queremos espiar el metodo find y ejecutamos nuestro propio metodo */
+      spyOn(Pins, "findById").and.callFake((id, callBack) => {
+        callBack(true, data);
+      });
+
+      request.get(
+        "http://localhost:3000/api/21312",
+        (error, response, body) => {
+          expect(response.statusCode).toBe(500);
+          done();
+        }
+      );
+    });
   });
+
+  // describe()
+
 });
