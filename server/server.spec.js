cb6d4f5fdd1839b8fc29d8801e587bed19e9da65
ArielAyala <arielayalamail@gmail.com>
2020-11-02 20:07:22 -0300

    Pruebas en el servidor: Probando el método POST (request to server)

--- server/server.spec.js
+++ server/server.spec.js
@@ -4,7 +4,9 @@ const http = require("http");
 const PinsRouter = require("./routes/pins");
 const Pins = require("./models/Pins");
 const request = require("request");
+const axios = require("axios");
 const app = express();
+var requestPromise = require("request-promise-native");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -24,7 +26,6 @@ describe("Testing Router", () => {
   });
 
   describe("GET", () => {
-
     // GET 200
     it("200 and find pin", (done) => {
       const data = [{ id: 1 }];
@@ -51,47 +52,51 @@ describe("Testing Router", () => {
         done();
       });
     });
-
-    // Get 200 FindById
-    it("200 and findById pin", (done) => {
-      const data = { id: 1 };
-      /* Dentro del Objeto Pins queremos espiar el metodo findById y ejecutamos nuestro propio metodo */
-      spyOn(Pins, "findById").and.callFake((id, callBack) => {
-        data.param = id;
-        callBack(false, data);
-      });
-
-      request.get(
-        "http://localhost:3000/api/21312",
-        (error, response, body) => {
-          expect(response.statusCode).toBe(200);
-          expect(error).toBe(null);
-          expect(JSON.parse(response.body)).toEqual({ id: 1, param: "21312" });
-          expect(JSON.parse(response.body).id).toEqual(1);
-          expect(JSON.parse(body).param).toEqual("21312");
-          done();
-        }
-      );
-    });
-
-    //GET 500 FindById
-    it("500", (done) => {
-      const data = [{ id: 1 }];
-      /* Dentro del Objeto Pins queremos espiar el metodo find y ejecutamos nuestro propio metodo */
-      spyOn(Pins, "findById").and.callFake((id, callBack) => {
-        callBack(true, data);
-      });
-
-      request.get(
-        "http://localhost:3000/api/21312",
-        (error, response, body) => {
-          expect(response.statusCode).toBe(500);
-          done();
-        }
-      );
-    });
   });
 
-  // describe()
+  describe("POST", () => {
+    it("200", (done) => {
+      const post = [
+        {
+          title: "Platzi",
+          author: "Platzi",
+          description: "Platzi rules",
+          percentage: 0,
+          tags: [],
+          assets: [
+            {
+              title: "Platzi",
+              description: "description",
+              readed: false,
+              url: "http://platzi.com",
+            },
+          ],
+        },
+      ];
 
+      spyOn(Pins, "create").and.callFake((pin, callBack) => {
+        callBack(false, {});
+      });
+
+      spyOn(requestPromise, "get").and.returnValue(
+        Promise.resolve(
+          '<title>Platzi</title><meta name="description" content="Platzi rules">'
+        )
+      );
+
+      const assets = [{ url: "http://platzi.com" }];
+
+      axios
+        .post("http://localhost:3000/api", {
+          title: "title",
+          author: "author",
+          description: "description",
+          assets,
+        })
+        .then((res) => {
+          expect(res.status).toBe(200);
+          done();
+        });
+    });
+  });
 });
