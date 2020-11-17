39640d92d144e18ce98f1d7e4a0e42890d59bc31
ArielAyala <arielayalamail@gmail.com>
2020-11-17 19:13:10 -0300

    Probando el caso de exito al guardar un elemento en el servidor.

--- src/app/components/form/form.component.spec.ts
+++ src/app/components/form/form.component.spec.ts
@@ -1,12 +1,12 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-import { FormArray, ReactiveFormsModule } from "@angular/forms";
-import { MatSnackBar } from "@angular/material";
-import { of } from "rxjs";
-import { NavigationService } from "src/app/services/navigation.service";
-import { RepositoryService } from "src/app/services/repository.service";
 
 import { FormComponent } from "./form.component";
+import { RepositoryService } from "src/app/services/repository.service";
+import { of } from "rxjs";
+import { NavigationService } from "src/app/services/navigation.service";
+import { MatSnackBar } from "@angular/material";
+import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { ReactiveFormsModule, FormArray } from "@angular/forms";
 
 class RepositoryServiceStub {
   savePins() {
@@ -55,9 +55,8 @@ fdescribe("FormComponent", () => {
     expect(component).toBeTruthy();
   });
 
-  describe("When component is initializated", () => {
-    it("should create the forms", () => {
-      // console.log(Object.keys(component.firstFormGroup.controls));
+  describe("When component is initilizated", () => {
+    it("Should create the forms", () => {
       expect(Object.keys(component.firstFormGroup.controls)).toEqual([
         "title",
         "author",
@@ -77,7 +76,38 @@ fdescribe("FormComponent", () => {
       component.addAsset();
       component.addAsset();
 
+      console.log(Object.keys(assets.controls));
       expect(Object.keys(assets.controls)).toEqual(["0", "1"]);
+    });
+  });
+
+  describe("When deleteAsset", () => {
+    it("Should remove the form control", () => {
+      const assets = <FormArray>component.secondFormGroup.get("assets");
+
+      component.addAsset();
+      component.deleteAsset(0);
+
+      expect(Object.keys(assets.controls)).toEqual([]);
+    });
+  });
+
+  describe("When savePins is executed", () => {
+    it("Should navigate to pins view", () => {
+      const navigate = spyOn((<any>component).navigate, "goToPins");
+      const open = spyOn((<any>component).snackBar, "open").and.callThrough();
+
+      component.savePin();
+
+      expect(navigate).toHaveBeenCalled();
+      console.log("working");
+      expect(open).toHaveBeenCalledWith(
+        "Your pin is saved, Redirecting ...",
+        "Cool!",
+        {
+          duration: 2000,
+        }
+      );
     });
   });
 });
