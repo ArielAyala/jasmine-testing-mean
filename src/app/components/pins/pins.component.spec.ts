bc3125a075937d849d837d9a198af177a56b892d
ArielAyala <arielayalamail@gmail.com>
2020-11-22 19:44:11 -0300

    Configurando el TestBed de el componente PINs

--- src/app/components/pins/pins.component.spec.ts
+++ src/app/components/pins/pins.component.spec.ts
@@ -1,16 +1,56 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatSnackBar } from "@angular/material";
+import { of, Subject } from "rxjs";
+import { PINS } from "src/app/services/mocks/pins";
+import { RepositoryService } from "src/app/services/repository.service";
 
-import { PinsComponent } from './pins.component';
+import { PinsComponent } from "./pins.component";
+import { PinsService } from "./pins.service";
 
-describe('PinsComponent', () => {
+class RepositoryServiceStub {
+  observer = new Subject();
+  getPins() {
+    return this.observer;
+  }
+
+  resolvePins() {
+    this.observer.next(JSON.parse(JSON.stringify(PINS)));
+  }
+
+  updatePin() {
+    return of(true);
+  }
+}
+
+class MatSnackBarStub {
+  open() {}
+}
+
+class PinsServiceStub {
+  observer = new Subject();
+  $actionObserver = this.observer.asObservable();
+  resolve(action) {
+    return this.observer.next(action);
+  }
+}
+
+fdescribe("PinsComponent", () => {
   let component: PinsComponent;
   let fixture: ComponentFixture<PinsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PinsComponent ]
-    })
-    .compileComponents();
+      declarations: [PinsComponent],
+      providers: [
+        { provide: RepositoryService, useClass: RepositoryServiceStub },
+        { provide: MatSnackBar, useClass: MatSnackBarStub },
+        { provide: PinsService, useClass: PinsServiceStub },
+      ],
+      imports: [ReactiveFormsModule],
+      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +59,7 @@ describe('PinsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
