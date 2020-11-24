202703a546cc6c041ca78256a102c93430e41bd8
ArielAyala <arielayalamail@gmail.com>
2020-11-24 19:53:41 -0300

    Probando navegacion

--- src/app/services/repository.service.spec.ts
+++ src/app/services/repository.service.spec.ts
@@ -1,12 +1,41 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { NavigationService } from "./navigation.service";
 
-import { RepositoryService } from './repository.service';
+import { RepositoryService } from "./repository.service";
 
-describe('RepositoryService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe("RepositoryService", () => {
+  let service: NavigationService;
 
-  it('should be created', () => {
-    const service: RepositoryService = TestBed.get(RepositoryService);
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      providers: [NavigationService],
+      imports: [RouterTestingModule],
+    })
+  );
+
+  beforeEach(() => {
+    service = TestBed.get(NavigationService);
+  });
+
+  it("should be created", () => {
     expect(service).toBeTruthy();
   });
+
+  it("should navigate to pins", () => {
+    const navigate = spyOn((<any>service).router, "navigate");
+
+    service.goToPins();
+
+    expect(navigate).toHaveBeenCalledWith(["/app/pins"]);
+  });
+
+  it("should navigate to edit mode", () => {
+    const navigate = spyOn((<any>service).router, "navigate");
+
+    service.goToEditMode();
+
+    expect(navigate).toHaveBeenCalledWith(["/app/add"]);
+  });
+
 });
