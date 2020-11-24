1495689f33f909e9bdf98010ffba7c865dfbd40f
ArielAyala <arielayalamail@gmail.com>
2020-11-24 20:46:10 -0300

    Test de metodos GET

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,12 +1,50 @@
-import { TestBed } from '@angular/core/testing';
+import { getTestBed, TestBed } from "@angular/core/testing";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
 
-import { ApiService } from './api.service';
+import { ApiService } from "./api.service";
+import { environment } from "src/environments/environment";
 
-describe('ApiService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe("ApiService", () => {
+  let service: ApiService;
+  let injector: TestBed;
+  let httpMock: HttpTestingController;
 
-  it('should be created', () => {
-    const service: ApiService = TestBed.get(ApiService);
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      providers: [ApiService],
+      imports: [HttpClientTestingModule],
+    })
+  );
+
+  beforeEach(() => {
+    injector = getTestBed();
+    service = TestBed.get(ApiService);
+    httpMock = injector.get(HttpTestingController);
+  });
+
+  afterAll(() => {
+    injector = null;
+    service = null;
+    httpMock = null;
+  });
+
+  it("should be created", () => {
     expect(service).toBeTruthy();
+  });
+
+  describe("GET", () => {
+    it("Should execute GET", () => {
+      const result = "testing";
+      service.get("/test").subscribe((response) => {
+        expect(response).toBe(result);
+      });
+
+      const req = httpMock.expectOne(environment.apiEndpoint + "/test");
+      expect(req.request.method).toBe("GET");
+      req.flush(result);
+    });
   });
 });
